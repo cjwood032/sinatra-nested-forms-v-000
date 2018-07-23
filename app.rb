0e51a3a9e921040aa22236791ccf7a8af91d87f8
Christopher Wood <cjwood032@gmail.com>
2018-07-23 15:23:59 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,11 +6,15 @@ module FormsLab
       erb :root
     end
     get '/new' do
-      erb :'pirates/new'  
+      erb :'pirates/new'
     end
     post '/pirates' do
       @pirate=Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
     end
-    
+
   end
 end
